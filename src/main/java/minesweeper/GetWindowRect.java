d5899604b50da56759c63e685eb73d7256aa173f
Quanhao Li <ql523@nyu.edu>
2016-12-16 13:33:44 -0500

    Full commit

--- src/main/java/minesweeper/GetWindowRect.java
+++ src/main/java/minesweeper/GetWindowRect.java
@@ -6,42 +6,42 @@ import com.sun.jna.win32.*;
 
 public class GetWindowRect {
 
-   public interface User32 extends StdCallLibrary {
-      User32 INSTANCE = (User32) Native.loadLibrary("user32", User32.class,
-               W32APIOptions.DEFAULT_OPTIONS);
+    public interface User32 extends StdCallLibrary {
 
-      HWND FindWindow(String lpClassName, String lpWindowName);
+        User32 INSTANCE = (User32) Native.loadLibrary("user32", User32.class,
+                W32APIOptions.DEFAULT_OPTIONS);
 
-      int GetWindowRect(HWND handle, int[] rect);
-   }
+        HWND FindWindow(String lpClassName, String lpWindowName);
 
-   public static int[] getRect(String windowName) throws WindowNotFoundException,
-            GetWindowRectException {
-      HWND hwnd = User32.INSTANCE.FindWindow(null, windowName);
-      if (hwnd == null) {
-         throw new WindowNotFoundException("", windowName);
-      }
+        int GetWindowRect(HWND handle, int[] rect);
+    }
 
-      int[] rect = {0, 0, 0, 0};
-      int result = User32.INSTANCE.GetWindowRect(hwnd, rect);
-      if (result == 0) {
-         throw new GetWindowRectException(windowName);
-      }
-      return rect;
-   }
+    public static int[] getRect(String windowName) throws WindowNotFoundException, GetWindowRectException {
+        HWND hwnd = User32.INSTANCE.FindWindow(null, windowName);
+        if (hwnd == null) {
+            throw new WindowNotFoundException("", windowName);
+        }
 
-   @SuppressWarnings("serial")
-   public static class WindowNotFoundException extends Exception {
-      public WindowNotFoundException(String className, String windowName) {
-         super(String.format("Window null for className: %s; windowName: %s", 
-                  className, windowName));
-      }
-   }
+        int[] rect = {0, 0, 0, 0};
+        int result = User32.INSTANCE.GetWindowRect(hwnd, rect);
+        if (result == 0) {
+            throw new GetWindowRectException(windowName);
+        }
+        return rect;
+    }
 
-   @SuppressWarnings("serial")
-   public static class GetWindowRectException extends Exception {
-      public GetWindowRectException(String windowName) {
-         super("Window Rect not found for " + windowName);
-      }
-   }
+    @SuppressWarnings("serial")
+    public static class WindowNotFoundException extends Exception {
+        public WindowNotFoundException(String className, String windowName) {
+            super(String.format("Window null for className: %s; windowName: %s",
+                    className, windowName));
+        }
+    }
+
+    @SuppressWarnings("serial")
+    public static class GetWindowRectException extends Exception {
+        public GetWindowRectException(String windowName) {
+            super("Window Rect not found for " + windowName);
+        }
+    }
 }
--- src/main/java/minesweeper/Minesweeper.java
+++ src/main/java/minesweeper/Minesweeper.java
@@ -12,87 +12,84 @@ import java.io.File;
 import java.io.IOException;
 
 public class Minesweeper extends Thread {
-	
-	boolean readyToRun=false;
-	
-	int topX, topY, botX, botY;
-	
-	final int offsetX=15;
-	final int offsetY=-101;
-	
-	final int botOffsetX=15;
-	final int botOffsetY=15;
-	
-	MineReader mineInterface;
-	MineAI mineAI;
-	
-	
-	public void run()
-	{
-		try {Thread.sleep(300);
-		} catch (InterruptedException e1) {}
-		
-		System.out.println("starting ai...");
-		
-		locate();
-		
-		if(readyToRun)
-		{
-			try {
-				updateFieldImage();
-			} catch (AWTException e) {
-				e.printStackTrace();
-			}
-			
-			mineAI = new MineAI(this);
-			
-			mineAI.initialize();
-			mineAI.mainLoop();
-			//mineAI.test();
-		}
-	}
-	
-	public void updateFieldImage() throws AWTException
-	{
-		int height = (botY-botOffsetY)-(topY-offsetY);
-		int width = (botX-botOffsetX)-(topX+offsetX);
-		
-		//System.out.println("window of size: " +width +"x" + height + "found");
-		
-		Rectangle window = new Rectangle(topX+offsetX,topY-offsetY,width,height);
-		Robot robot = new Robot();
-		
-		BufferedImage screenShot = robot.createScreenCapture(window);
-		
-		try{
-			File image = new File("screenshot.png");
-			ImageIO.write(screenShot, "png", image);
-		} catch (IOException e){}
-	}
-	
-	public void locate()
-	{
-		System.out.println("Locating minesweeper window... \nPlease do not move the window.");
-		
-		String windowName = "Minesweeper X";
-	    int[] rect;
-	    readyToRun=true;
-	    try {
-	    	rect = GetWindowRect.getRect(windowName);
-	    	System.out.println("corner locations for minesweeper: \n"+
-	    		   windowName+ Arrays.toString(rect));
-	    	topX=rect[0];
-	    	topY=rect[1];
-	    	botX=rect[2];
-	    	botY=rect[3];
-	         
-	    	}
-	    catch (GetWindowRect.WindowNotFoundException e){
-	    	System.out.println("Please open minesweeper and try again");
-	    	readyToRun=false;
-	    }
-	    catch (GetWindowRect.GetWindowRectException e){
-	    	readyToRun=false;
-	    }
-	}
+
+    public MineStarter mineStarter;
+    boolean readyToRun = false;
+
+    int topX, topY, botX, botY;
+
+    final int offsetX = 15;
+    final int offsetY = -101;
+
+    final int botOffsetX = 15;
+    final int botOffsetY = 15;
+
+    MineReader mineInterface;
+    HillClimbingMineAI hillClimbingMineAI;
+    PatternMineAI patternMineAI;
+
+
+    public void run() {
+        try {
+            Thread.sleep(300);
+        } catch (InterruptedException e1) {
+        }
+
+        System.out.println("Starting AI...");
+        locate();
+
+        if (readyToRun) {
+            try {
+                updateFieldImage();
+            } catch (AWTException e) {
+                e.printStackTrace();
+            }
+            if(mineStarter.input == 1) {
+                hillClimbingMineAI = new HillClimbingMineAI(this);
+                hillClimbingMineAI.initialize();
+                hillClimbingMineAI.mainLoop();
+            }
+            if(mineStarter.input == 2){
+                patternMineAI = new PatternMineAI(this);
+                patternMineAI.initialize();
+                patternMineAI.mainLoop();
+            }
+        }
+    }
+
+    public void updateFieldImage() throws AWTException {
+        int height = (botY - botOffsetY) - (topY - offsetY);
+        int width = (botX - botOffsetX) - (topX + offsetX);
+
+        Rectangle window = new Rectangle(topX + offsetX, topY - offsetY, width, height);
+        Robot robot = new Robot();
+
+        BufferedImage screenShot = robot.createScreenCapture(window);
+
+        try {
+            File image = new File("State.png");
+            ImageIO.write(screenShot, "png", image);
+        } catch (IOException e) {}
+    }
+
+    public void locate() {
+        System.out.println("Locating Minesweeper...");
+        String windowName = "Minesweeper X";
+        int[] rect;
+        readyToRun = true;
+        try {
+            rect = GetWindowRect.getRect(windowName);
+            System.out.println("Corner of Minesweeper:\n" + windowName + Arrays.toString(rect));
+            topX = rect[0];
+            topY = rect[1];
+            botX = rect[2];
+            botY = rect[3];
+
+        } catch (GetWindowRect.WindowNotFoundException e) {
+            System.out.println("Open Minesweeper and try again");
+            readyToRun = false;
+        } catch (GetWindowRect.GetWindowRectException e) {
+            readyToRun = false;
+        }
+    }
 }
